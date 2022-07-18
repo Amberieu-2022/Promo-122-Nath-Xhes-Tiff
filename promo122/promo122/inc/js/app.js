f081b85cdb2b18efa3e2a96fd449d8f6b53dab67
XhesikaMilaqi <97739823+XhesikaMilaqi@users.noreply.github.com>
2022-07-18 16:22:54 +0200

    final

--- promo122/promo122/inc/js/app.js
+++
@@ -1,8 +0,0 @@
-const burgerMenu = document.getElementById('burger-menu');
-const nav = document.getElementById('menu');
-
-burgerMenu.addEventListener('click', function(e){
-    e.preventDefault();
-    nav.classList.toggle('show');
-    burgerMenu.classList.toggle('open');
-});
